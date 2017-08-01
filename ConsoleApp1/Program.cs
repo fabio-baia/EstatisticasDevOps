f4e134a9f59018b9d97ade7fe3132e3f42964e7a
Fabio Cezar Baía <fabio.baia@benner.com.br>
2017-08-01 14:49:44 -0300

    Movida logica de obtencao de dados da api para classe separada

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Text;
 
 namespace ConsoleApp1
@@ -12,30 +11,13 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-            var ids = new List<int>();
             var detalhesBuilds = new List<BuildDetalhes>();
-
-            string authInfo = "usuario" + ":" + "senha";
-            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://mga-tc001:6081/app/rest/builds/?locator=buildType:Ag_CSharp_Release46");
-            request.Headers.Add("Authorization", "Basic " + authInfo);
-            request.Accept = "application/json";
-
-            WebResponse response = request.GetResponse();
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-
-            Coisa coisa = JsonConvert.DeserializeObject<Coisa>(responseFromServer);
-
-            var builds = coisa.Build;
+            var builds = TeamCityAPIClient.ObterBuilsdDaRelease();
 
             foreach (var b in builds)
             {
-                ids.Add(b.Id);
-                var hora = ObterHoraBuild(b.Id);
-                var estatisticas = ObterPropriedades(b.Id);
+                var hora = TeamCityAPIClient.ObterHoraBuild(b.Id);
+                var estatisticas = TeamCityAPIClient.ObterPropriedades(b.Id);
                 b.Property = estatisticas;
 
                 Console.WriteLine(JsonConvert.SerializeObject(b));
@@ -70,9 +52,6 @@ namespace ConsoleApp1
             }
 
             ExportarCSV(detalhesBuilds);
-
-            reader.Close();
-            response.Close();
 
             Console.ReadKey();
         }
@@ -112,48 +91,6 @@ namespace ConsoleApp1
             }
 
             File.WriteAllText("C:/Temp/DevOps.csv", csv.ToString());
-        }
-
-        public static List<Property> ObterPropriedades(int buildId)
-        {
-            string authInfo = "usuario" + ":" + "senha";
-            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
-
-            var url = string.Format("http://mga-tc001:6081/app/rest/builds/id:{0}/statistics", buildId);
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Headers.Add("Authorization", "Basic " + authInfo);
-            request.Accept = "application/json";
-
-            WebResponse response = request.GetResponse();
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-
-            Build coisa = JsonConvert.DeserializeObject<Build>(responseFromServer);
-
-            return coisa.Property;
-        }
-
-        public static string ObterHoraBuild(int buildId)
-        {
-            string authInfo = "usuario" + ":" + "senha";
-            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
-
-            var url = string.Format("http://mga-tc001:6081/app/rest/builds/id:{0}", buildId);
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Headers.Add("Authorization", "Basic " + authInfo);
-            request.Accept = "application/json";
-
-            WebResponse response = request.GetResponse();
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-
-            Build coisa = JsonConvert.DeserializeObject<Build>(responseFromServer);
-
-            //DateTimeOffset result = DateTimeOffset.Parse(coisa.StartDate, CultureInfo.GetCultures(CultureTypes.AllCultures).Where(x => x.));
-
-            return coisa.StartDate;
         }
     }
 }
---
+++ ConsoleApp1/TeamCityAPIClient.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class TeamCityAPIClient
+    {
+        public static List<Build> ObterBuilsdDaRelease()
+        {
+            string authInfo = "usuario" + ":" + "senha";
+            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://mga-tc001:6081/app/rest/builds/?locator=buildType:Ag_CSharp_Release46");
+            request.Headers.Add("Authorization", "Basic " + authInfo);
+            request.Accept = "application/json";
+
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+
+            Coisa coisa = JsonConvert.DeserializeObject<Coisa>(responseFromServer);
+
+            reader.Close();
+            response.Close();
+
+            return coisa.Build;
+        }
+
+        public static List<Property> ObterPropriedades(int buildId)
+        {
+            string authInfo = "usuario" + ":" + "senha";
+            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+
+            var url = string.Format("http://mga-tc001:6081/app/rest/builds/id:{0}/statistics", buildId);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("Authorization", "Basic " + authInfo);
+            request.Accept = "application/json";
+
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+
+            Build coisa = JsonConvert.DeserializeObject<Build>(responseFromServer);
+
+            reader.Close();
+            response.Close();
+
+            return coisa.Property;
+        }
+
+        public static string ObterHoraBuild(int buildId)
+        {
+            string authInfo = "usuario" + ":" + "senha";
+            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+
+            var url = string.Format("http://mga-tc001:6081/app/rest/builds/id:{0}", buildId);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("Authorization", "Basic " + authInfo);
+            request.Accept = "application/json";
+
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+
+            Build coisa = JsonConvert.DeserializeObject<Build>(responseFromServer);
+
+            //DateTimeOffset result = DateTimeOffset.Parse(coisa.StartDate, CultureInfo.GetCultures(CultureTypes.AllCultures).Where(x => x.));
+
+            reader.Close();
+            response.Close();
+
+            return coisa.StartDate;
+        }
+    }
+}
